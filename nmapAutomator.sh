87741a8c84b3a602e5310c1aa5d920a87700e084
21y4d <26864473+21y4d@users.noreply.github.com>
2019-03-17 15:21:05 +0300

    Group by IP
    Grouped all results in a folder by IP, fixed a couple of bugs

--- nmapAutomator.sh
+++ nmapAutomator.sh
@@ -198,6 +198,7 @@ if [ -z `echo "${basicPorts}"` ]; then
         echo -e "${YELLOW}Making a script scan on all ports"
         echo -e "${NC}"
         $nmapType -sCV -p`echo "${allPorts}"` -oN nmap/Full_$1.nmap $1
+	assignPorts $1
 else
 	cmpPorts $1
 	if [ -z `echo "${extraPorts}"` ]; then
@@ -213,6 +214,7 @@ else
         	echo -e "${YELLOW}Making a script scan on extra ports: `echo "${extraPorts}" | sed 's/,/, /g'`"
         	echo -e "${NC}"
         	$nmapType -sCV -p`echo "${extraPorts}"` -oN nmap/Full_$1.nmap $1
+		assignPorts $1
 	fi
 fi
 
@@ -502,6 +504,12 @@ else
 fi
 
 if [[ "$2" =~ ^(Quick|Basic|UDP|Full|Vulns|Recon|All)$ ]]; then
+	if [[ ! -d $1 ]]; then
+	        mkdir $1
+	fi
+
+	cd $1
+	
 	if [[ ! -d nmap/ ]]; then
 	        mkdir nmap/
 	fi
