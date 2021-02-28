712da5e2796c47152b2a2a667226b742fa8a924a
Carlos Ibáñez <caribpa@outlook.com>
2021-02-28 17:15:23 +0100

    Refactor bashisms

--- nmapAutomator.sh
+++ nmapAutomator.sh
@@ -595,12 +595,12 @@ footer() {
         echo -e "${NC}"
         echo -e ""
 
-        if (($SECONDS > 3600)); then
+        if [ ${SECONDS} -gt 3600 ]; then
                 let "hours=SECONDS/3600"
                 let "minutes=(SECONDS%3600)/60"
                 let "seconds=(SECONDS%3600)%60"
-        elif (($SECONDS > 60)); then
                 echo -e "${YELLOW}Completed in ${hours} hour(s), ${minutes} minute(s) and ${seconds} second(s)"
+        elif [ ${SECONDS} -gt 60 ]; then
                 let "minutes=(SECONDS%3600)/60"
                 let "seconds=(SECONDS%3600)%60"
                 echo -e "${YELLOW}Completed in ${minutes} minute(s) and ${seconds} second(s)"
